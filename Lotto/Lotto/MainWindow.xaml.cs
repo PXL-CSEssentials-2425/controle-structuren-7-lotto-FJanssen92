6f233c36dc8cf658b58b110d9cf8a91f6cc91f65
FJanssen92 <12402449@student.pxl.be>
2024-10-09 21:28:54 +0200

    Oefening klaar

---
+++ Lotto/Lotto/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lotto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random lotto = new Random();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < 7; i++)
+            {
+                int lottoGetal = lotto.Next(1, 46);
+                sb.Append($"Getal {i}: {lottoGetal}\r\n");
+                string result = sb.ToString();
+                resultTextBox.Text = result;
+            }
+
+
+
+
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+        }
+    }
+}
